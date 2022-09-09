a4cd65455bf7feb855f231ade8334b9f75c80f47
esadakman <esadd26@gmail.com>
2022-09-09 19:32:48 +0300

    user profile & image & update

--- users/forms.py
+++ users/forms.py
@@ -2,17 +2,32 @@ from django import forms
 from .models import UserProfile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
 
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('image',)
-        # exclude= ('user',)
-#exclude dememizin sebebi biz oluşturduğumuz kullanıcının userProfileini oluşturmak iistiyoruz
-#bunu yaptığımız içinde views kısmında register olan kullanıcıyıya userprofile ı kaydettik 
-#eğer fieldsları all olark tanımlasaydık kayıtlı olan kullanıcılardan seçim yaptıracaktık
+# class UserProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = UserProfile
+#         fields = ('image',)
+        # exclude= ('user',) 
 
 class UserRegisterForm(UserCreationForm):
+    # class Meta:
+    #     model = User
+    #     fields = ('username', 'email')
+    email = forms.EmailField()
+
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,30 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404 
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate,login ,logout
-from .forms import UserRegisterForm, UserProfileForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+# , UserProfileForm
 from django.contrib import messages 
 from django.contrib.auth.decorators import login_required
 def user_register(request):
-    # form = UserRegisterForm()    
-    form_user = UserRegisterForm()
-    form_profile = UserProfileForm()
-    if request.method=="POST":
-        form_user = UserRegisterForm(request.POST)
-        form_profile = UserProfileForm(request.POST)#dosyaları almak için
-        if form_user.is_valid():
-            user = form_user.save()
-            profile = form_profile.save(commit=False)
-            profile.user = user
-            profile.save()
-
-            login(request,user)
-            
-            # return redirect('home')
-    context = {
-        'register_form': form_user,
-        'form_profile': form_profile,
-    }
-    return render(request, 'users/register.html', context)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})  
 
 def user_login(request):
     form = AuthenticationForm(request, data=request.POST)
@@ -46,4 +37,65 @@ def user_logout(request):
 
 @login_required
 def user_profile(request):
-     return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.userprofile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('user_profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.userprofile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+    # form_user = UserRegisterForm()
+    # form_profile = UserProfileForm()
+    # if request.method=="POST":
+    #     form_user = UserRegisterForm(request.POST)
+    #     form_profile = UserProfileForm(request.POST)#dosyaları almak için
+    #     if form_user.is_valid():
+    #         user = form_user.save()
+    #         profile = form_profile.save(commit=False)
+    #         profile.user = user
+    #         profile.save()
+
+    #         login(request,user)
+            
+    #         # return redirect('home')
+    # context = {
+    #     'register_form': form_user,
+    #     'form_profile': form_profile,
+    # }
+    # return render(request, 'users/register.html', context)
