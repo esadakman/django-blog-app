d33c67cd5ac5ebc1a3951d66bacc3f8057d4e2fd
esadakman <esadd26@gmail.com>
2022-09-13 13:08:49 +0300

    comment area bug fixed

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            output_size = (500, 500)
+            output_size = (800, 800)
             img.thumbnail(output_size)
             img.save(self.image.path)
 
