ff61f0dadeeef167fd27cc172e44899c7adfd6a3
esadakman <esadd26@gmail.com>
2022-09-13 16:08:15 +0300

    blog image

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True, default='blog_default.png',upload_to='user_directory_path')  
+    image = models.ImageField( default='blog_default.png',upload_to='user_directory_path')  
     blog_view = models.IntegerField(default=0, blank=True) 
     likes = models.ManyToManyField(User, blank=True, related_name="collected_votes") 
     blog_comment = models.PositiveIntegerField(default=0)
